f1098f4dd24ade9dae9005582dc188e2cce1bb2c
Bradley Shively <bshively@gmail.com>
2018-06-15 16:01:11 -0400

    Add more ch2 exercise solutions

--- ch2/ex2.7/count.cpp
+++ ch2/ex2.7/count.cpp
@@ -5,16 +5,36 @@ using namespace std;
 
 int main()
 {
+    // ex2.7
     for(int i = 10; i > -6; i--) {
         cout << i << endl;
     }
 
     cout << endl;
 
+    // ex2.8
     int product = 1;
     for (int i = 1; i < 10; i++) {
         product *= i;
     }
     cout << product << endl;
+
+    // ex2.9
+    cout << "Enter first number: ";
+    int firstNumber;
+    cin >> firstNumber;
+
+    cout << "Enter second number: ";
+    int secondNumber;
+    cin >> secondNumber;
+
+    if (firstNumber > secondNumber) {
+        cout << firstNumber;
+    } else {
+        cout << secondNumber;
+    }
+
+    cout << " is larger.\n";
+
     return 0;
 }
